8a03b5a672461837ee03bc7620fb7cea504f72f8
JaviDelgado101 <122196793+JavierDelgado101@users.noreply.github.com>
2023-01-27 17:47:32 +0000

    Main and ItemsnPrice

---
+++ ItemsnPrice.java
@@ -0,0 +1,16 @@
+class ItemsnPrice {
+double price;
+String item;
+    double Food() {
+        String = "" + "-";
+        double price *= .075;
+        
+    }
+}
+class ItemPrice {
+    public static void main(String args[]) {
+    Items chsburger = new Items();   
+    chsburger.Food() = 4.50;
+    System.out.println(Food.chsburger);
+    }
+}
---
+++ Project.java
@@ -0,0 +1,7 @@
+//MainFrame
+class Project {
+    public static void main(String args[]) {
+    //Insert array of food items w/ price - 2x dimen    
+    }
+    
+}

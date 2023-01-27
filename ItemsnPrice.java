d14270fffaf24f29b2b1d6ad20fd47140d8bf85a
JaviDelgado101 <122196793+JavierDelgado101@users.noreply.github.com>
2023-01-27 19:00:06 +0000

    New Changes

--- ItemsnPrice.java
+++ ItemsnPrice.java
@@ -1,17 +1,16 @@
 class ItemsnPrice {
-double price;
-String item;
-    public double Food(double price, String item) {
-        item = "" + "-";
-        price = price * .075;
-        return item;
-        //Fix
-    }
+    String price;
+    String item;
 }
 class ItemPrice {
     public static void main(String args[]) {
+    
     ItemsnPrice chsburger = new ItemsnPrice();   
+    
     chsburger.item = "Cheeseburger";
-    System.out.println(chsburger.item);
+    chsburger.price = "$4.50";
+    
+    System.out.print(chsburger.item + " ");
+    System.out.println("- " + chsburger.price );
     }
 }

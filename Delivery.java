24290c8c91317a2441e58a385f66453ba6bfc24f
JaviDelgado101 <eatmorejr128@gmail.com>
2023-01-27 15:41:26 -0800

    Fix Bug

--- Delivery.java
+++ Delivery.java
@@ -1,5 +1,5 @@
 public class Delivery {
-    public static void main(String args[]) throws java.IO.IOException  {
+    public static void main(String args[]) throws java.io.IOException  {
         System.out.println("Order:");
         //Include items ordered by user
     }

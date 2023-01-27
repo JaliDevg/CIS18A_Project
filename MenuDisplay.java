709d86aca26d2b7ade7ab45703602e7381f9e8b6
JaviDelgado101 <eatmorejr128@gmail.com>
2023-01-27 15:24:44 -0800

    Working Full Menu Display

--- MenuDisplay.java
+++ MenuDisplay.java
@@ -6,12 +6,12 @@ public class MenuDisplay {
         {"French Fries", "$2.05"},
         {"Ground Beef Burrito", "$10.00"},
         {"Chicken Taco", "$4.25"},
-        {"12\" Pepperoni Pizza $12.50"},
+        {"12\" Pepperoni Pizza", "$12.50"},
         {"10 Piece - Southern Fried Jumbo Shrimp", "$10.50"},
         {"Fish and Chips", "$11.50"}
     };
     int a, b;
-    for(a=0; a < 5; a++) {
+    for(a=0; a < 8; a++) {
         for(b=0; b < 2; b++)
             System.out.print(food[a][b] + " ");
         System.out.println();

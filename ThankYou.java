baba7c79d7df0110ea1a72e4839349d6ac752705
JaviDelgado101 <122196793+JavierDelgado101@users.noreply.github.com>
2023-01-27 00:04:10 -0800

    Delete ThankYou.java

--- ThankYou.java
+++
@@ -1,7 +0,0 @@
-public class ThankYou {
-    public static void main(String args[]) {
-        System.out.println("Thank you for your order!");
-        System.out.println("Have a great day!");
-        System.exit(0);
-    }
-}

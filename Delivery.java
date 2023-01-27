3d99d696f0bb7d1c6f1c71c862f4ca87651edb5c
JaviDelgado101 <122196793+JavierDelgado101@users.noreply.github.com>
2023-01-27 23:34:18 +0000

    Original

---
+++ Delivery.java
@@ -0,0 +1,6 @@
+public class Delivery {
+    public static void main(String args[]) throws java.IO.IOException  {
+        System.out.println("Order:");
+        //Include items ordered by user
+    }
+}

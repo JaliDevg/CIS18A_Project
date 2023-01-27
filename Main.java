87dd16136c7d1a9071cb77af42cf5d120460b382
JaviDelgado101 <122196793+JavierDelgado101@users.noreply.github.com>
2023-01-27 23:08:29 +0000

    Reorganized

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) throws java.io.IOException {
+    Scanner sc = new Scanner (System.in);
+    System.out.println();
+    }
+}
--- Project.java
+++
@@ -1,7 +0,0 @@
-//MainFrame
-class Project {
-    public static void main(String args[]) {
-    //Insert array of food items w/ price - 2x dimen    
-    }
-    
-}

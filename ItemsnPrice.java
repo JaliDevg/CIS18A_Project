34ddd076578b5d2229e360b39d628f1f8f14f4ba
JaviDelgado101 <122196793+JavierDelgado101@users.noreply.github.com>
2023-01-27 19:29:35 +0000

    Patch | Testing

--- ItemsnPrice.java
+++ ItemsnPrice.java
@@ -2,15 +2,25 @@ class ItemsnPrice {
     String price;
     String item;
 }
-class ItemPrice {
+class Menu {
     public static void main(String args[]) {
+    //Constuctors
+    ItemsnPrice chsbgr = new ItemsnPrice();   
+    ItemsnPrice htDg = new ItemsnPrice(); 
+
+    //Cheeseburger Variables | Print
+    chsbgr.item = "Cheeseburger";
+    chsbgr.price = "$5.50";
+    System.out.print(chsbgr.item + " ");
+    System.out.println("- " + chsbgr.price );
+
+    //HotDog Variables | Print
+    htDg.item = "Hot Dog";
+    htDg.price = "$4.00";
+    System.out.print(htDg.item + " ");
+    System.out.println("- " + htDg.price );
+    //French Fries Variables
     
-    ItemsnPrice chsburger = new ItemsnPrice();   
     
-    chsburger.item = "Cheeseburger";
-    chsburger.price = "$4.50";
-    
-    System.out.print(chsburger.item + " ");
-    System.out.println("- " + chsburger.price );
     }
 }
---
+++ Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String args[]) {
+    String food[][] = {
+        {"Cheeseburger", "$5.50"},
+        {"Hot Dog", "$4.00"},
+        {"French Fries", "$2.05"}
+    };
+    int a, b;
+    for(a=0; a < 3; a++) {
+        for(b=0; b < 2; b++)
+            System.out.print(food[a][b] + " ");
+        System.out.println(); 
+        System.out.println();
+        }    
+    }
+}

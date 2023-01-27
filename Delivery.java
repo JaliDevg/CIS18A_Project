32c0b70330271bf29e58a40ec815087be15c20ca
JaviDelgado101 <122196793+JavierDelgado101@users.noreply.github.com>
2023-01-27 15:42:02 -0800

    Delete Delivery.java

--- Delivery.java
+++
@@ -1,6 +0,0 @@
-public class Delivery {
-    public static void main(String args[]) throws java.io.IOException  {
-        System.out.println("Order:");
-        //Include items ordered by user
-    }
-}

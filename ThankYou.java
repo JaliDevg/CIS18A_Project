e8fb85e32dd877df42054a93c1f8fdb089b7aa55
JaliDevg <eatmorejr128@gmail.com>
2023-01-26 22:33:03 -0800

    Welcome and Goodbye

---
+++ ThankYou.java
@@ -0,0 +1,7 @@
+public class ThankYou {
+    public static void main(String args[]) {
+        System.out.println("Thank you for your order!");
+        System.out.println("Have a great day!");
+        System.exit(0);
+    }
+}
---
+++ Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome {
+    public static void main(String args[]) 
+    throws java.io.IOException {
+        System.out.println("Welcome!");
+        System.out.println("Place an order To Go:");
+        //Implement Swing GUI - To click button "Place an order To Go"
+    }
+}

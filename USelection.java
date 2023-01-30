3eca5ecce8ce683f119ccb323e0e12af5667926b
Javier Delgado <122196793+javdel3@users.noreply.github.com>
2023-01-30 23:16:54 +0000

    fix

--- USelection.java
+++ USelection.java
@@ -2,8 +2,14 @@ public class USelection {
     //Constructors
     String price;
     String item;
+    int num;
+    void print() throws java.io.IOException {
+        if ((int) System.in.read() == num) {
+            System.out.println("Order:");
+        }
+    }    
 }
-class Slct {
+class Slct extends USelection{
     public static void main(String args[]) throws java.io.IOException {
         USelection chsBurger = new USelection();
         USelection htDg = new USelection();
@@ -13,32 +19,36 @@ class Slct {
         USelection pepPizza = new USelection();
         USelection sfjShrimp = new USelection();
         USelection fhChip = new USelection();
-    String i;
     //Items Variables
         //Cheeseburger
+    chsBurger.num = 1;
     chsBurger.item = "Cheeseburger";
     chsBurger.price = "$5.50";
         //Hot Dog
+    htDg.num = 2;
     htDg.item = "Hot Dog";
     htDg.price = "$4.00";
         //French Fries
+    fF.num = 3;
     fF.item = "French Fries";
     fF.price = "$5.50";
-    switch(i) {
-        case 0:
-            if ((String) System.in.read(chsBurger.item)) {
+    boolean ignition = true;
+    int nitems = 1;
+    while(ignition == true) {
+        switch(nitems) {
+        case '1':
             System.out.print(chsBurger.item);
-            System.out.println(chsBurger.price);
-            }
+            System.out.println(" " + chsBurger.price);
             break;
-        case 1:
-            if ((String) System.in.read(htDg.item)) {
+        case '2':
             System.out.print(chsBurger.item);
-            System.out.println(chsBurger.price);
-            }
+            System.out.println(" " + chsBurger.price);
             break;
-
+        default:
+            System.out.println("Please Select an Item.");
+        }
     }
+    
     
     // Refer to Imput Char example chapter 3 '?' and Aidan's code
     }

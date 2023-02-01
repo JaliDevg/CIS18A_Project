1f4b4ff70896ec28529d5f74605f86c354f4bd21
javdel3 <eatmorejr128@gmail.com>
2023-01-31 22:03:30 -0800

    Text Main import

--- delivery/Address.java
+++ delivery/Address.java
@@ -1,13 +1,16 @@
 package delivery;
 import java.util.*;
 public class Address {
-    public static void main(String[] args) throws java.io.IOException, IllegalStateException {
+    public void userAddress() throws java.io.IOException, IllegalStateException {
         System.out.println("Insert Delivery Address: ");
         Scanner readtxt = new Scanner(System.in);
         String typein = readtxt.nextLine();
-        if (typein.nextLine(System.in)) {
+        System.out.println("Estimated Delivery Time: 30 minutes.");
+        /*
+         if (typein.nextLine(System.in)) {
             System.out.println("Estimated Delivery Time: 30 minutes.");
         }
+         */
         //Make program type 1 line of text until 'ENTER' is clicked
     }
 }

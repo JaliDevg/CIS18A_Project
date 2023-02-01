fc242affca9201bbd26232f5f5f7529813c643a0
javdel3 <eatmorejr128@gmail.com>
2023-01-31 21:15:41 -0800

    Set up allotted Hours and Clock *Needs time format adjust *Set up delivery time

--- TestMain.java
+++ TestMain.java
@@ -2,7 +2,7 @@ import delivery.*;
 public class TestMain {
     public static void main(String args[]) {
         Hours timedisplay = new Hours();
-        timedisplay = Hours();
-        System.out.println(timedisplay);
+        timedisplay.userTime();
+        System.out.println();
     }
 }

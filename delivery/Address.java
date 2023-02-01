92e110ce0bb8f50befcaf10a178752893ed7c80e
javdel3 <eatmorejr128@gmail.com>
2023-01-31 21:56:45 -0800

    Setup Outprint

--- delivery/Address.java
+++ delivery/Address.java
@@ -1,12 +1,13 @@
 package delivery;
 import java.util.*;
 public class Address {
-    public static void main(String args[]) throws java.io.IOException  {
+    public static void main(String[] args) throws java.io.IOException, IllegalStateException {
         System.out.println("Insert Delivery Address: ");
         Scanner readtxt = new Scanner(System.in);
-        String typein = readtxt;
-        if (typein == "Send Food Order") {
+        String typein = readtxt.nextLine();
+        if (typein.nextLine(System.in)) {
             System.out.println("Estimated Delivery Time: 30 minutes.");
         }
+        //Make program type 1 line of text until 'ENTER' is clicked
     }
 }

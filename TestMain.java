8206f5d02434742b4e198dfa0482ef099237b9db
javdel3 <eatmorejr128@gmail.com>
2023-01-31 20:38:29 -0800

    new change package possible

---
+++ TestMain.java
@@ -0,0 +1,8 @@
+import delivery.*;
+public class TestMain {
+    public static void main(String args[]) {
+        Hours timedisplay = new Hours();
+        timedisplay = Hours();
+        System.out.println(timedisplay);
+    }
+}

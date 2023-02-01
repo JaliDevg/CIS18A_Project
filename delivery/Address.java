cfa0a6d64da1910f0bde6ea78440823cf140b016
javdel3 <eatmorejr128@gmail.com>
2023-01-31 21:47:43 -0800

    Output setup

--- delivery/Address.java
+++ delivery/Address.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Address {
     public static void main(String args[]) throws java.io.IOException  {
         System.out.println("Insert Delivery Address: ");
-        Scanner readtxt = new Scanner();
-        String typein = readtxt.System.in;
+        Scanner readtxt = new Scanner(System.in);
+        String typein = readtxt;
         if (typein == "Send Food Order") {
             System.out.println("Estimated Delivery Time: 30 minutes.");
         }

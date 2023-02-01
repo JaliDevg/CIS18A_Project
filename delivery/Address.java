3a75623856e30ac7bf06ad8ab73821abe13e031b
javdel3 <eatmorejr128@gmail.com>
2023-01-31 21:25:44 -0800

    Address setup

--- delivery/Address.java
+++ delivery/Address.java
@@ -1,6 +1,12 @@
 package delivery;
+import java.util.*;
 public class Address {
     public static void main(String args[]) throws java.io.IOException  {
-        System.out.println("Order:");
+        System.out.println("Insert Delivery Address: ");
+        Scanner readtxt = new Scanner();
+        String typein = readtxt.System.in;
+        if (typein == "Send Food Order") {
+            System.out.println("Estimated Delivery Time: 30 minutes.");
+        }
     }
 }

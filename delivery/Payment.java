f8a903314340804bd4b8f01caefd70adfb6895a9
Javi <122196793+javdel3@users.noreply.github.com>
2023-02-03 17:35:19 +0000

    Card Info revise

--- delivery/Payment.java
+++ delivery/Payment.java
@@ -7,16 +7,9 @@ public class Payment {
     public long cardRead(int radix) {
         Scanner scanner = new Scanner(System.in);
         cardinfo = scanner.nextLong(radix);
-        scanner.close();
-        return cardinfo;
-    }
-}
-class UserPayment extends Payment {
-    public void userPayAmt() throws IllegalArgumentException {
-        System.out.print("Enter Card Number Here: ");
-        Payment UserPay = new Payment();
         //Summary TotalPay = new Summary();
         //System.out.println(TotalPay.total + " charged to: " + UserPay.cardRead(10));
-        System.out.println("__ charged to: " + UserPay.cardRead(16));
+        scanner.close();
+        return cardinfo;
     }
 }

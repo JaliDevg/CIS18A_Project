847bbe6e8439f65a68b16fd336d68735df571182
Javi <122196793+javdel3@users.noreply.github.com>
2023-02-06 20:31:25 +0000

    Welcome setup

--- startend/Welcome.java
+++ startend/Welcome.java
@@ -6,14 +6,20 @@ public class Welcome {
         //Print Introduction
         System.out.println();
         System.out.println("\"REGULAR RESTAURANT\"");
-        System.out.println("Monday - Sunday: 11 a.m. - 10 p.m.");
+        System.out.println("Business Hours:\nMonday - Sunday: 11 a.m. - 10 p.m.");
         System.out.println("Welcome!");
         //Print Current Time and Time
         Date currentTime = new Date();
         System.out.println(currentTime);
         if(currentTime >= ) {
-            System.out.println()
+            System.out.println("We are Open!");
+            System.out.println("Place an order To Go:");
         }
-        System.out.println("Place an order To Go:");
+        else {
+            System.out.println("Sorry, we are closed.");
+            System.out.println("Come again at an available time.");
+            System.out.println("Hours: 11 a.m. - 10 p.m.");
+        }
+        
     }
 }

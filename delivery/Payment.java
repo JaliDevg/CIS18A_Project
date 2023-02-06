b6fb993b9a1a0f44710221208204e55734f48022
javdel3 <eatmorejr128@gmail.com>
2023-02-05 22:27:04 -0800

    Branch manual merge

---
+++ delivery/Payment.java
@@ -0,0 +1,23 @@
+package delivery;
+
+import order.CalcTotal;
+import java.util.*;
+
+public class Payment {
+    long cardinfo;
+    public long cardRead(int radix) {
+        Scanner scanner = new Scanner(System.in);
+        cardinfo = scanner.nextLong(radix);
+        scanner.close();
+        return cardinfo;
+    }
+}
+class UserPayment extends Payment {
+    public void userPayAmt() throws IllegalArgumentException {
+        System.out.print("Enter Card Number Here: ");
+        Payment UserPay = new Payment();
+        //Summary TotalPay = new Summary();
+        //System.out.println(TotalPay.total + " charged to: " + UserPay.cardRead(10));
+        System.out.println("__ charged to: " + UserPay.cardRead(16));
+    }
+}

d3a5aa38b21bc8c61517d6afb07bce27e22d4b7b
javdel3 <eatmorejr128@gmail.com>
2023-02-05 21:51:34 -0800

    Revert "Card Info revise"
    This reverts commit f8a903314340804bd4b8f01caefd70adfb6895a9.

--- delivery/Payment.java
+++ delivery/Payment.java
@@ -7,9 +7,16 @@ public class Payment {
     public long cardRead(int radix) {
         Scanner scanner = new Scanner(System.in);
         cardinfo = scanner.nextLong(radix);
-        //Summary TotalPay = new Summary();
-        //System.out.println(TotalPay.total + " charged to: " + UserPay.cardRead(10));
         scanner.close();
         return cardinfo;
+    }
+}
+class UserPayment extends Payment {
+    public void userPayAmt() throws IllegalArgumentException {
+        System.out.print("Enter Card Number Here: ");
+        Payment UserPay = new Payment();
+        //Summary TotalPay = new Summary();
+        //System.out.println(TotalPay.total + " charged to: " + UserPay.cardRead(10));
+        System.out.println("__ charged to: " + UserPay.cardRead(16));
     }
 }

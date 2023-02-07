3a5c0ffd52556dfb2733ce88518502c3a19778db
javdel3 <eatmorejr128@gmail.com>
2023-02-06 20:06:38 -0800

    Add comments

--- startend/Welcome.java
+++ startend/Welcome.java
@@ -3,27 +3,29 @@ import java.time.LocalTime;
 import java.time.format.*;
 
 public class Welcome {
+    public void checkTime() {
+         //Time Package: Local Time and Format (Reference - https://docs.oracle.com/en/java/javase/19/docs/api/java.base/java/time/LocalTime.html)
+        LocalTime currentTime = LocalTime.now();
+            //Print Current Time
+        System.out.println("Current Time: " + currentTime.format(DateTimeFormatter.ofPattern("hh:mm a")));
+            //Check user's current time
+        LocalTime am = LocalTime.of(11, 0);
+        LocalTime pm = LocalTime.of(22, 0);
+            //Output based on the user's current time
+        if (currentTime.isAfter(am) && currentTime.isBefore(pm)) {
+            System.out.println("We are Open!");
+            System.out.println("Place an order To Go:");
+        } else {
+            System.out.println("Sorry, we are closed.");
+            System.out.println("You may still place your order. However it will not be delivered until we open again.");
+        }
+    }
     public void greetWlc() {
         //Print Introduction
         System.out.println();
         System.out.println("\"REGULAR RESTAURANT\"");
         System.out.println("Business Hours:\nMonday - Sunday: 11 a.m. - 10 p.m.");
         System.out.println("Welcome!");
-        //
-        LocalTime currentTime = LocalTime.now();
-            //Print Current Time
-        System.out.println("Current Time: " + currentTime.format(DateTimeFormatter.ofPattern("hh:mm a")));
-            //Check user's current time
-        LocalTime startTime = LocalTime.of(11, 0);
-        LocalTime endTime = LocalTime.of(22, 0);
-
-        if (currentTime.isAfter(startTime) && currentTime.isBefore(endTime)) {
-            System.out.println("We are Open!");
-            System.out.println("Place an order To Go:");
-        } else {
-            System.out.println("Sorry, we are closed.");
-            System.out.println("Come back at an available time.");
-            System.out.println("Hours: 11 a.m. - 10 p.m.");
-        }
+       
     }
 }

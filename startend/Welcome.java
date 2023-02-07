26564682d22b1283f96e840c3e294017c0789990
javdel3 <eatmorejr128@gmail.com>
2023-02-06 16:47:10 -0800

    Tim Check Publish v2.7.2

--- startend/Welcome.java
+++ startend/Welcome.java
@@ -1,5 +1,6 @@
 package startend;
-import java.util.Date;
+import java.time.LocalTime;
+import java.time.format.*;
 
 public class Welcome {
     public void greetWlc() {
@@ -8,18 +9,21 @@ public class Welcome {
         System.out.println("\"REGULAR RESTAURANT\"");
         System.out.println("Business Hours:\nMonday - Sunday: 11 a.m. - 10 p.m.");
         System.out.println("Welcome!");
-        //Print Current Time and Time
-        Date currentTime = new Date();
-        System.out.println(currentTime);
-        if(currentTime >= ) {
+        //
+        LocalTime currentTime = LocalTime.now();
+            //Print Current Time
+        System.out.println("Current Time: " + currentTime.format(DateTimeFormatter.ofPattern("hh:mm a")));
+            //Check user's current time
+        LocalTime startTime = LocalTime.of(11, 0);
+        LocalTime endTime = LocalTime.of(22, 0);
+
+        if (currentTime.isAfter(startTime) && currentTime.isBefore(endTime)) {
             System.out.println("We are Open!");
             System.out.println("Place an order To Go:");
-        }
-        else {
+        } else {
             System.out.println("Sorry, we are closed.");
-            System.out.println("Come again at an available time.");
+            System.out.println("Come back at an available time.");
             System.out.println("Hours: 11 a.m. - 10 p.m.");
         }
-        
     }
 }
